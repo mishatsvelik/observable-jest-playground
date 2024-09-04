9086b09ae3edede9347f3b8d789b58c95dc27b62
Misha Tsvelik <misha.tsvelik@hotmail.com>
2024-09-04 20:15:24 +0100

    added 'observable-plot-dependency-library-cell'

--- tests/observable-notebook.test.ts
+++ tests/observable-notebook.test.ts
@@ -10,9 +10,16 @@ it('observable-cell', async () => {
     expect(Number.isInteger(test)).toBe(true)
 });
 
-it('observable-dependency-library-cell', async () => {
+it('observable-d3-dependency-library-cell', async () => {
     const runtime = new Runtime();
     const main = runtime.module(notebook);  
     const test = await main.value("d3_test"); //notebook cell code: d3_test = d3.utcFormat("%Y")(new Date);
     assert.deepStrictEqual(test, "2024");
+});
+
+it('observable-plot-dependency-library-cell', async () => {
+    const runtime = new Runtime();
+    const main = runtime.module(notebook);  
+    const test = await main.value("plot_test"); //notebook cell code: d3_test = d3.utcFormat("%Y")(new Date);
+    assert.deepStrictEqual(test, "https://observablehq.com/d/5ef68b6d8020c0c7");
 });
